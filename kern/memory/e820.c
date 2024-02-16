053434ec5bdfb9a79c255acd916a6be964219f80
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2024-02-16 13:37:18 +0100

    Trying to fix bug where memory map prints zero

--- kern/memory/e820.c
+++ kern/memory/e820.c
@@ -1,12 +1,13 @@
 #include "e820.h"
 
+E820MemoryBlock array[100];
+// E820MemoryBlock* array = (E820MemoryBlock*)0x2FFF;
 void detect_memory()
 {
-    E820MemoryBlock array[100];
     e820(array);
-
+    
     for (uint16_t i = 0; i < entry_count; i++) 
     {
-        printf("%lld\r\n", array[i].Length);
+        printf("%d: %llx | %llx\r\n", i, array[i].Base, array[i].Length);
     }
 }
